2ff8c7aee15cd9594e7eb02483ea7a1fdb874dda
Gabriel-Floricel <gabrielfloricel@yahoo.com>
2021-06-11 12:08:48 +0300

    cards align

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,38 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import getProductsForCategory from '../productList';
-import Card from 'react-bootstrap/Card';
-import './Category.css';
+import getProductsForCategory from "../productList";
+import Card from "react-bootstrap/Card";
+import "./Category.css";
 
 function Category() {
-    const {categoryId} = useParams();
+  const { categoryId } = useParams();
 
-    const [recipeData, setRecipeData] = useState([]);
+  const [recipeData, setRecipeData] = useState([]);
 
-    useEffect(() => {
-        setRecipeData(getProductsForCategory(categoryId));
-    }, [categoryId])
+  useEffect(() => {
+    setRecipeData(getProductsForCategory(categoryId));
+  }, [categoryId]);
 
-    const card = recipeData.map((product) => (
-      <Card className="Card">
-        <Card.Img className='img' variant='top' src={product.image} />
-        <Card.Body>
-          <Card.Title>{product.name}</Card.Title>
-          <Card.Text> {product.description} </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-            <small>{product.price}</small>
-        </Card.Footer>
-      </Card>
-    ));
-    console.log(card);
+  const card = recipeData.map((product) => (
+    <Card className='Card'>
+      <Card.Img className='img' variant='top' src={product.image} />
+      <Card.Body>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text> {product.description} </Card.Text>
+      </Card.Body>
+      <Card.Footer className='card-footer'>
+        <small>{product.price}</small>
+      </Card.Footer>
+    </Card>
+  ));
+  console.log(card);
 
-
-    return (
-        <div className='Category'>
-                {card}
-        </div>
-    )
+  return <div className='Category'>{card}</div>;
 }
 
 export default Category;
